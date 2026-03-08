fdfd83ccffb27a742438191dfb52c214423557c1
Vietanh1509 <chivietanh999@gmail.com>
2026-03-08 20:33:00 +0700

    Add files via upload

---
+++ test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    printf("dang tai tin nhan...\n");
+
+    sleep(2);
+    system("afplay LoveLanguage.mp3");
+
+
+    printf("\n");
+
+
+
+
+
+
+
+
+
+
+
+
+    getchar();
+    return 0;
+}
